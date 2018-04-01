589271951d9103f6ba38643214d9366d30ad7c5e
GRUNT-16 <36847856+GRUNT-16@users.noreply.github.com>
2018-04-01 22:04:19 +0300

    некоторые доработки

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var popup = document.querySelector(".trip-info");
+var arrivalDate = popup.querySelector(".arrival-date-input");
+var departureDate = popup.querySelector(".departure-date-input")
 var tripInfoButton = document.querySelector(".trip-info-open-close-button");
 var tripInfoAnimationLeft = document.querySelector(".trip-info-animation-left");
 tripInfoButton.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.toggle("trip-info-show");
-})
+});
